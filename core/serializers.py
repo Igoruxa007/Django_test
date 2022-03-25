ef2e0d2475dc84ae5e420aee3dd3f98bb394a5c8
Igoruxa007 <74563405+Igoruxa007@users.noreply.github.com>
2022-03-25 11:21:50 +0300

    added view post and autorization

---
+++ core/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Post
+from django.contrib.auth.models import User
+
+
+class PostSerializer(serializers.ModelSerializer):
+
+    author = serializers.SlugRelatedField(slug_field="username", queryset=User.objects.all())
+
+    class Meta:
+        model = Post
+        fields = ("id", "h1", "title", "slug", "description", "content", "image", "created_at", "author")
+        lookup_field = 'slug'
+        extra_kwargs = {
+            'url': {'lookup_field': 'slug'}
+        }

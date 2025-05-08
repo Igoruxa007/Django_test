d826f26397dbc6dbe04e69d43d2804077e6757dc
Игорь Бородий <igoruxa007@gmail.com>
2025-05-08 23:04:12 +0400

    Changed ckeditor to ckeditor_5

--- api/settings.py
+++ api/settings.py
@@ -26,8 +26,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'core',
-    'ckeditor',
-    'ckeditor_uploader',
+    'django_ckeditor_5',
     'django_extensions',
     'drf_yasg',
 ]
@@ -41,53 +40,10 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# CKEDITOR CONFIGURATION
-####################################
-
-CKEDITOR_JQUERY_URL = 'https://ajax.googleapis.com/ajax/libs/ \
-                        jquery/2.2.4/jquery.min.js'
-
-CKEDITOR_UPLOAD_PATH = 'uploads/'
-CKEDITOR_IMAGE_BACKEND = 'pillow'
-CKEDITOR_CONFIGS = {
-    'default': {
-        'removePlugins': 'stylesheetparser',
-        'allowedContent': True,
-        'toolbar_Full': [
-            [
-                'Styles', 'Format', 'Bold', 'Italic', 'Underline', 'Strike',
-                'Subscript', 'Superscript', '-', 'RemoveFormat',
-            ],
-            ['Image', 'Flash', 'Table', 'HorizontalRule'],
-            ['TextColor', 'BGColor'],
-            ['Smiley', 'sourcearea', 'SpecialChar'],
-            ['Link', 'Unlink', 'Anchor'],
-            [
-                'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-',
-                'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter',
-                'JustifyRight', 'JustifyBlock', '-', 'BidiLtr',
-                'BidiRtl', 'Language',
-            ],
-            [
-                'Source', '-', 'Save', 'NewPage', 'Preview', 'Print',
-                '-', 'Templates',
-            ],
-            [
-                'Cut', 'Copy', 'Paste', 'PasteText',
-                'PasteFromWord', '-', 'Undo', 'Redo',
-            ],
-            ['Find', 'Replace', '-', 'SelectAll', '-', 'Scayt'],
-            ['Maximize', 'ShowBlocks'],
-        ],
-    },
-}
-
-###################################
-
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
-    }
+    },
 }
 
 MIDDLEWARE = [
